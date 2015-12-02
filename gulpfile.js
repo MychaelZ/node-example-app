1fb2e12a47ae6cb1fff87d4b16ef03256f6b92f9
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-12-01 19:32:31 -0800

    add package.json scripts

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const babel = require('gulp-babel');
 const jasmine = require('gulp-jasmine');
 const watch = require('gulp-watch');
 const coffee = require('gulp-coffee');
+const nodemon = require('gulp-nodemon');
 
 gulp.task('test:e2e', function () {
   gulp
@@ -18,59 +19,27 @@ gulp.task('test:e2e', function () {
 
 gulp.task('compile:e2e', function () {
   gulp
-    .src('spec-coffee/e2e/helpers/*.coffee')
-    .pipe(watch('spec-coffee/e2e/helpers/*.coffee'))
+    .src('spec-coffee/**/*.coffee', {base: './spec-coffee'})
+    .pipe(watch('spec-coffee/**/*.coffee'))
     .pipe(coffee({bare: true}))
-    .pipe(gulp.dest('spec/e2e/helpers'));
-
-  gulp
-    .src('spec-coffee/e2e/book-spec/*.coffee')
-    .pipe(watch('spec-coffee/e2e/book-spec/*.coffee'))
-    .pipe(coffee({bare: true}))
-    .pipe(gulp.dest('spec/e2e/book-spec'));
-
-  gulp
-    .src('spec-coffee/e2e/patron-spec/*.coffee')
-    .pipe(watch('spec-coffee/e2e/patron-spec/*.coffee'))
-    .pipe(coffee({bare: true}))
-    .pipe(gulp.dest('spec/e2e/patron-spec'));
-
-  gulp
-    .src('spec-coffee/*.coffee')
-    .pipe(watch('spec-coffee/*.coffee'))
-    .pipe(coffee({bare: true}))
-    .pipe(gulp.dest('spec'));    
+    .pipe(gulp.dest('spec'));
 });
 
-gulp.task('transpile:books', function () {
+gulp.task('transpile', function () {
   gulp
-    .src(['books/**/*.js'])
-    .pipe(watch(['books/**/*.js']))
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(gulp.dest('build/books'));
-});
-
-gulp.task('transpile:patrons', function () {
-  gulp
-    .src(['patrons/**/*.js'])
-    .pipe(watch(['patrons/**/*.js']))
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(gulp.dest('build/patrons'));
-});
-
-gulp.task('transpile:server', function () {
-  gulp
-    .src(['*.js', '!gulpfile.js'])
-    .pipe(watch(['*.js', '!gulpfile.js']))
+    .src(['./**/*.js', '!node_modules/**', '!build/**', '!spec/**', '!gulpfile.js'], {base: '.'})
+    .pipe(watch(['./**/*.js', '!node_modules/**', '!build/**', '!spec/**', '!gulpfile.js']))
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('build', ['compile:e2e', 'transpile:server', 'transpile:books', 'transpile:patrons']);
+gulp.task('run', () => {
+  nodemon({
+    script: 'build/main.js'
+  });
+});
 
+gulp.task('start', ['default', 'run']);
+gulp.task('default', ['compile:e2e', 'transpile']);
